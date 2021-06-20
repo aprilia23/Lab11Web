1d5a9ae584a8e5548ee46349b8b75f7498ee0199
Aprilia Nur Saadah <54062259+aprilia23@users.noreply.github.com>
2021-06-20 14:27:04 +0700

    Add files via upload

---
+++ Config/page.php
@@ -0,0 +1,28 @@
+<?php
+namespace App\Controllers;
+class Page extends BaseController
+
+{
+ public function about()
+ {
+ return view('about', [
+ 'title' => 'Halaman Abot',
+ 'content' => 'Ini adalah halaman abaut yang menjelaskan tentang isi halaman ini.']);
+ }
+ public function contact()
+ {
+ return view('contact', [
+ 'title' => 'Halaman Contact',
+ 'content' => 'Ini adalah halaman abaut yang menjelaskan tentang isi halaman ini.']); 
+ }
+ public function artikel()
+ {
+ return view('artikel', [
+ 'title' => 'Halaman artikel',
+ 'content' => 'Ini adalah halaman abaut yang menjelaskan tentang isi halaman ini.']);
+ }
+ public function tos()
+{
+ echo "ini halaman Term of Services";
+}
+}
